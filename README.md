7941e8d80b76a359a62d3e85c45eed4b534ff643
Berend Dekens <cyberwizzard@gmail.com>
2015-04-08 10:35:02 +0200

    Initial commit including the Rabobank CSV parser; the ING Bank MT940 parser will follow shortly

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # mt940
+
 An composer extension to the MT940 parser from the jejik/mt940 library that fixes parsing ING Bank statements in the MT940 format and adds support for Rabobank CSV statements
+
+## Installation
+
+You can install cyberwizzard/MT940 using Composer. You can read more about Composer and its main repository at
+[http://packagist.org](http://packagist.org "Packagist"). First install Composer for your project using the instructions on the
+Packagist home page, then define your dependency on cyberwizzard/MT940 in your `composer.json` file.
+
+```json
+    {
+        "require": {
+            "cyberwizzard/mt940": ">=0.3"
+        }
+    }
+```
+
+This library follows the [PSR-0 standard](https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-0.md). You will need
+a PSR-0 compliant autoloader to load the cyberwizzard/MT940 classes. Composer provides one for you in your `vendor/.composer/autoload.php`.
+
+## Usage
+
+```php
+<?php
+
+use Jejik\MT940\Reader;
+use cyberwizzard\MT940\Parser\RabobankCSV;
+
+$reader = new Reader();
+
+// Load the Rabobank CSV parser
+$reader->addParser( 'RabobankCSV', 'cyberwizzard\MT940\Parser\RabobankCSV' );
+// Append the list of default bank parsers (optional)
+$reader->addParsers( $reader->getDefaultParsers() );
+
+$statements = $reader->getStatements(file_get_contents('mt940.txt'));
+
+foreach ($statements as $statement) {
+    echo $statement->getOpeningBalance()->getAmount() . "\n";
+
+    foreach ($statement->getTransactions() as $transaction) {
+        echo $transaction->getAmount() . "\n";
+    }
+
+    echo $statement->getClosingBalance()->getAmount() . "\n";
+}
+```
+
+For more information, see the [jejik/MT940 repository](https://github.com/sandermarechal/jejik-mt940).
+
+## Disclaimer
+Use this parser at your own risk. While the utmost care is taken to design parsers which are fully compliant with the
+official bank statement formats, most of these seem to have quirks.
+
+This parser is in use in a private project and as such is tested regularly.
---
+++ lib/Parser/RabobankCSV.php
@@ -0,0 +1,130 @@
+<?php
+
+/*
+ * This file is meant as an extension of the Jejik\MT940 library to provide CSV parsing for Rabobank CSV exports
+ *
+ * Copyright (c) 2012 Sander Marechal <s.marechal@jejik.com>
+ * Licensed under the MIT license
+ *
+ * For the full copyright and license information, please see the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace cw\MT940\Parser;
+use Jejik\MT940\Parser\AbstractParser;
+
+/**
+ * Parser for Rabobank CSV documents
+ *
+ * @author Berend Dekens <cyberwizzard@gmail.com>
+ */
+class RabobankCSV extends AbstractParser
+{
+    // Fields as specified by the 2013 CSV format from the Rabobank
+    const F_REKENINGNUMMER_REKENINGHOUDER = 0;
+    const F_MUNTSOORT = 1;
+    const F_RENTEDATUM = 2;
+    const F_BY_AF_CODE = 3;
+    const F_BEDRAG = 4;
+    const F_TEGENREKENING = 5;
+    const F_NAAR_NAAM = 6;
+    const F_BOEKDATUM = 7;
+    const F_BOEKCODE = 8;
+
+    const F_OMSCHRIJVING1 = 10;
+    const F_OMSCHRIJVING2 = 11;
+    const F_OMSCHRIJVING3 = 12;
+    const F_OMSCHRIJVING4 = 13;
+    const F_OMSCHRIJVING5 = 14;
+    const F_OMSCHRIJVING6 = 15;
+
+    /**
+     * Test if the document is a Rabobank CSV formatted file
+     *
+     * @param string $text
+     * @return bool
+     */
+    public function accept($text)
+    {
+        $line = strtok($text, "\n");
+        $fields = str_getcsv($line);
+        return (count($fields) == 19 && ($fields[self::F_BY_AF_CODE] == 'C' || $fields[self::F_BY_AF_CODE] == 'D'));    // Rabobank CSV uses 19 fields and the 4th field denotes Credit or Debet
+    }
+
+    /**
+     * Parse a CSV formatted text file from the Rabobank
+     */
+    public function parse($text) {
+        $statements = array();
+        $lines = explode("\n", $text);
+        foreach ($lines as $line) {
+            if(trim($line) == '') continue;
+            if ($statement = $this->parseLine($line)) {
+                $statements[] = $statement;
+            }
+        }
+
+        return $statements;
+    }
+
+    /**
+     * Parse a line of CSV
+     */
+    protected function parseLine($line) {
+        static $statement_number = 1;
+        $fields = str_getcsv($line);
+        if(count($fields) != 19)
+            throw new \RuntimeException('Not an Rabobank CSV statement: ' . $line);
+
+        $accountNumber = $fields[self::F_REKENINGNUMMER_REKENINGHOUDER];
+        $account = $this->reader->createAccount(null);
+
+        if (!($account instanceof Jejik\MT940\AccountInterface)) {
+        //    return null;
+        }
+
+        $account->setNumber($fields[self::F_REKENINGNUMMER_REKENINGHOUDER]);
+        $statement = $this->reader->createStatement($account, $statement_number);
+
+        if (!($statement instanceof StatementInterface)) {
+            //return null;
+        }
+
+        $statement->setAccount($account)
+                  ->setNumber($statement_number);
+
+        // Parse the amount
+        $amount = (float) $fields[self::F_BEDRAG];
+        if ($fields[self::F_BY_AF_CODE] == 'D') {
+            $amount *= -1;
+        }
+
+        // Parse dates
+        $valueDate = \DateTime::createFromFormat('Ymd', $fields[self::F_RENTEDATUM]);
+        $valueDate->setTime(0,0,0);
+
+        $bookDate = \DateTime::createFromFormat('Ymd', $fields[self::F_BOEKDATUM]);
+        $bookDate->setTime(0,0,0);
+
+        // Contract account
+        $caccount = $this->reader->createAccount(null);
+        $caccount->setNumber($fields[self::F_TEGENREKENING]);
+        $caccount->setName($fields[self::F_NAAR_NAAM]);
+
+        $description = $fields[self::F_OMSCHRIJVING1] . $fields[self::F_OMSCHRIJVING2] . $fields[self::F_OMSCHRIJVING3] . $fields[self::F_OMSCHRIJVING4] .
+                $fields[self::F_OMSCHRIJVING5] . $fields[self::F_OMSCHRIJVING6];
+        $transaction = $this->reader->createTransaction();
+        $transaction->setAmount($amount)
+                    ->setContraAccount($caccount)
+                    ->setValueDate($valueDate)
+                    ->setBookDate($bookDate)
+                    ->setDescription($this->description($description));
+
+        $statement->addTransaction($transaction);
+
+        // Increase the statement number
+        $statement_number = $statement_number + 1;
+
+        return $statement;
+    }
+}
